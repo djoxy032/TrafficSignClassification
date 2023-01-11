87a274b2181f8dbf0265d3a690eaf2cfa020760c
Djole <djordjeivanovic032@gmail.com>
2023-01-12 00:43:03 +0100

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Train neural networks to classify traffic signs',
+    author='Djole',
+    license='MIT',
+)
